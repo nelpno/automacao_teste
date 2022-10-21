54e810174712b5edfede1d43ebbab86cc115e298
nelpno <nelpno@gmail.com>
2022-10-21 07:26:46 -0300

    finalizacao de funcoes e loops

--- boleto_v4.py
+++ boleto_v4.py
@@ -4,66 +4,75 @@ from datetime import datetime
 import pyautogui
 import pyautogui as pa
 
-def pressionar_junto(letras):
+
+def tempo(segundos):
+    pyautogui.countdown(segundos)
 
 
-data_atual = datetime.today()
-data_em_texto = data_atual.strftime('%d-%m-%Y')
+def press_2_teclas(letra1, letra2):
+    pyautogui.hotkey(letra1, letra2)
+
+
+def apertar(letra):
+    pyautogui.press(letra)
+
+
+def escreve(texto):
+    pyautogui.write(texto)
+
+
+def imagem_clicar(arquivo):
+    achar_arquivo = pyautogui.locateOnScreen(arquivo)
+    pyautogui.click(achar_arquivo)
+
+
+def data_hoje():
+    data_atual = datetime.today()
+    data_em_texto = data_atual.strftime('%d-%m-%Y')
+    return data_em_texto
+
+
+def verificar_cor_pixel(x, y, r, g, b):
+    achou_cor = False
+
+    while not achou_cor:
+        if pa.pixelMatchesColor(x, y, (r, g, b)):
+            achou_cor = True
+
+    return True
+
 
 cliente = "Maxi Massas"
 valor_investido = "400,00"
 
-escrita_boleto = ("{}-{}".format(cliente, data_em_texto))
+escrita_boleto = ("{}-{}".format(cliente, data_hoje()))
 
-abrir_chrome = pyautogui.locateOnScreen('chrome_nelson.png')
-pyautogui.click(abrir_chrome)
-time.sleep(2)
-pyautogui.hotkey('ctrl', 'f4')
-pyautogui.countdown(1)
-pyautogui.write("https://business.facebook.com/ads/manager/billing_history/summary/?act=704083103754311")
-pyautogui.press("enter")
-pyautogui.countdown(5)
-adicionar_fundos = pyautogui.locateOnScreen('adicionar_fundos.png')
-pyautogui.click(adicionar_fundos)
-pyautogui.countdown(5)
-pyautogui.press('tab')
-pyautogui.hotkey('ctrl', 'a')
-pyautogui.press('backspace')
-pyautogui.write(valor_investido)
-clicar_boleto = pyautogui.locateOnScreen('boleto_bancario.png')
-pyautogui.click(clicar_boleto)
-adicionar_fundos_2 = pyautogui.locateOnScreen('adicionar_fundos_2.png')
-pyautogui.click(adicionar_fundos_2)
+imagem_clicar('chrome_nelson.png')
+tempo(2)
+press_2_teclas('ctrl', 'f4')
+tempo(1)
+escreve("https://business.facebook.com/ads/manager/billing_history/summary/?act=704083103754311")
+apertar("enter")
+tempo(5)
+imagem_clicar('adicionar_fundos.png')
+tempo(5)
+apertar('tab')
+press_2_teclas('ctrl', 'a')
+apertar('backspace')
+escreve(valor_investido)
+imagem_clicar('boleto_bancario.png')
+imagem_clicar('adicionar_fundos_2.png')
 
-cor_azul = False
+while not verificar_cor_pixel(2515, 771, 231, 243, 255):
+    verificar_cor_pixel(2515, 771, 231, 243, 255)
 
-while not cor_azul:
-    x, y = (2506, 771)
-    print(x, y)
-    time.sleep(2)
-    if pa.pixelMatchesColor(x, y, (231, 243, 255)):
-        cor_azul = True
+imagem_clicar('baixar.png')
+tempo(2)
+escreve(escrita_boleto)
+imagem_clicar('downloads.png')
+tempo(2)
+press_2_teclas('alt', 'l')
+tempo(1)
+imagem_clicar('x_boleto_feito.png')
 
-baixar_boleto = pyautogui.locateOnScreen('baixar.png')
-pyautogui.click(baixar_boleto)
-
-pyautogui.countdown(2)
-pyautogui.write(escrita_boleto)
-
-pasta_salvamento = pyautogui.locateOnScreen('downloads.png')
-pyautogui.click(pasta_salvamento)
-pyautogui.countdown(2)
-
-pyautogui.hotkey('alt', 'l')
-pyautogui.countdown(1)
-
-fechar_boleto_feito = pyautogui.locateOnScreen('x_boleto_feito.png')
-pyautogui.click(fechar_boleto_feito)
-
-# Box(left=3351, top=3, width=40, height=34) Nova Guia Chrome
-
-# pyautogui.moveTo(2009,50,2)
-
-# https://business.facebook.com/ads/manager/billing_history/summary/?act=704083103754311&pid=p1&business_id=205744063478775&page=billing_history&tab=summary&date=1663898400_1666317600
-
-# https://business.facebook.com/ads/manager/billing_history/summary/?act=704083103754311 LINK RESUMIDO
+# https://business.facebook.com/ads/manager/billing_history/summary/?act=704083103754311 LINK RESUMIDO ID DO CLIENTE
--- verificar_imagem_cursor.py
+++ verificar_imagem_cursor.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import time
 import PIL.ImageShow
 import pyautogui
@@ -10,17 +10,53 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
 
-def verificar_cor_eixo():
-    cor_branca = False
+def verificar_cor_eixo(r, g, b):
+    achou_cor = False
 
-    while cor_branca == False:
+    while not achou_cor:
         x, y = pyautogui.position()
         print(x, y)
         cor = pyautogui.pixel(x, y)
         print(cor)
         time.sleep(2)
-        if pa.pixelMatchesColor(x, y, (231,243,255)):
-            cor_branca = True
+        if pa.pixelMatchesColor(x, y, (r, g, b)):
+            achou_cor = True
+
+def verificar_cor_pixel(x, y, r, g, b):
+    achou_cor = False
+
+    while not achou_cor:
+        if pa.pixelMatchesColor(x, y, (r, g, b)):
+            achou_cor = True
+
+    return True
+
+# while not verificar_cor_eixo(255, 255, 255):
+verificar_cor_eixo(255, 255, 255)
 
 
-verificar_cor_eixo()
+def tempo(segundos):
+    pyautogui.countdown(segundos)
+
+
+def press_junto(letras):
+    pyautogui.hotkey(letras)
+
+
+def apertar(letra):
+    pyautogui.press(letra)
+
+
+def escreve(texto):
+    pyautogui.write(texto)
+
+
+def imagem_clicar(arquivo):
+    achar_arquivo = pyautogui.locateOnScreen(arquivo)
+    pyautogui.click(achar_arquivo)
+
+
+def data_hoje():
+    data_atual = datetime.today()
+    data_em_texto = data_atual.strftime('%d-%m-%Y')
+    return data_em_texto
